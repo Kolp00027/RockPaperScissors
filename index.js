2e20daf1832a329becf84726aa8d6d2f109d157f
Kolp00027 <agnerosie495@gmail.com>
2024-05-16 20:36:25 +0200

    first commit

--- index.js
+++ index.js
@@ -1,79 +1,65 @@
-console.log("Hello world");
-
-let humainScore = 0;
+let humanScore = 0;
 let computerScore = 0;
 
 function getComputerChoice() {
-  const choice = Math.random();
-  if (choice <= 0.33) {
-    console.log("Computer: pierre");
+  const choice = Math.floor(Math.random() * 3);
+
+  if (choice == 0) {
+    console.log("computer: pierre");
     return "pierre";
-  } else if (choice <= 0.66) {
-    console.log("Computer: papier");
+  } else if (choice == 1) {
+    console.log("Computer : papier");
     return "papier";
-  } else {
-    console.log("Computer: ciseaux");
+  } else if (choice == 2) {
+    console.log("Computer : ciseaux");
     return "ciseaux";
   }
 }
-getComputerChoice();
 
 function getHumanChoice() {
-  let humainChoice = prompt("choisir entre : pierre , papier, ciseaux ");
-  humainChoice == "";
-  if (humainChoice == "pierre") {
-    console.log("pierre");
-  } else if (humainChoice == "papier") {
-    console.log("papier");
-  } else {
-    console.log("ciseaux");
+  let choice = prompt("Choisir Entrez : pierre , papier, ciseaux");
+  if (choice == "pierre") {
+    console.log("humain : pierre");
+    return choice;
+  } else if (choice == "papier") {
+    console.log("humain : papier");
+    return choice;
+  } else if (choice == "ciseaux") {
+    console.log("humain : ciseaux");
+    return choice;
   }
 }
-getHumanChoice();
 
 function playRound(humanChoice, computerChoice) {
-  if (humanChoice == "pierre" && computerChoice == "pierre") {
-    console.log("match nul rejouer une partie");
-    return;
-  } else if (humanChoice == "pierre" && computerChoice == "ciseaux") {
-    console.log("L'utilisateur gagner la partie");
-    humainScore++;
-    return;
-  } else if (humanChoice == "ciseaux" && computerChoice == "papier") {
-    console.log("l'utilisateur remporter la partir");
-    humainScore++;
-    return;
-  } else if (humanChoice == "pierre" && computerChoice == "papier") {
-    console.log("l'ordinateur gagner sur le papier");
+  if (humanChoice == computerChoice) {
+    console.log(`vous etes a egalite ${humanChoice} et ${computerChoice} `);
+  } else if (
+    (humanChoice == "pierre" && computerChoice == "ciseaux") ||
+    (humanChoice == "ciseaux" && computerChoice == "paper") ||
+    (humanChoice == "papier" && computerChoice == "pierre")
+  ) {
+    console.log(`Vous avez gagnez ! ${humanChoice} bat ${computerChoice}`);
+    humanScore++;
+  } else {
+    console.log(`vous avez perdue ${computerChoice} bat ${humanChoice}`);
     computerScore++;
-    return;
-  } else if (humanChoice == "papier" && computerChoice == "papier") {
-    console.log("match null");
-    return;
-  } else if (humanChoice == "ciseaux" && computerChoice == "ciseaux") {
-    console.log("match null");
-    return;
   }
 }
 
 function playGame() {
-  humanScore = 0;
-  computerScore = 0;
-  for (let i = 0; i < 5; i++) {
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-  }
-  if (humanScore > computerScore) {
-    console.log("You win! You won " + humanScore + " times out of 5.");
-    return;
-  } else if (humanScore < computerScore) {
-    console.log("You lose! You won " + humanScore + " times out of 5.");
-    return;
-  } else {
-    console.log("We tied! You won " + humanScore + " times out of 5.");
-    return;
-  }
+  const humanSelection = getHumanChoice();
+  const computerSelection = getComputerChoice();
+  playRound(humanSelection, computerSelection);
 }
-
-playGame();
+for (let i = 0; i < 5; i++) {
+  playGame();
+}
+if (humanScore > computerScore) {
+  console.log(
+    `Vous avez gagné la partie avec un score de ${humanScore} à ${computerScore}`
+  );
+} else {
+  console.log(
+    `Vous avez perdu la partie avec un score de ${humanScore} à ${computerScore}`
+  );
+}
